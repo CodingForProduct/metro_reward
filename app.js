8cf7261b9903563c3a3627e95acd0079313f9540
niamurrell <niamurrell@gmail.com>
2017-07-04 17:26:21 -0700

    Set up initial app routes

---
+++ app.js
@@ -0,0 +1,24 @@
+// Include node modules
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var request = require("request");
+
+// Use node modules
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// ******* ROUTES ********
+
+// Root goes to sign-in page with sign-up button
+app.get("/", function(req, res) {
+	res.render("signin");
+});
+
+
+
+// Run app on localhost:3000
+app.listen(3000, function() {
+	console.log("App is running on Port 3000");
+});
