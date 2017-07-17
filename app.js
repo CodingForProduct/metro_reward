5c825a641f223ac2f9b5c2c140986129cd773762
niamurrell <niamurrell@users.noreply.github.com>
2017-07-16 22:03:48 -0700

    Add login modules & requirements to app.js

--- app.js
+++ app.js
@@ -2,8 +2,12 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
 var request = require("request");
 var Sequelize = require("sequelize");
+var flash = require("connect-flash");
+var passport = require("passport");
+var LocalStrategy = require('passport-local').Strategy;
 
 var connection = new Sequelize('metrodb', 'metroadmin', 'beer', {
 	host: 'localhost',
@@ -15,6 +19,44 @@ var connection = new Sequelize('metrodb', 'metroadmin', 'beer', {
 	 idle: 10000
  }
 });
+
+// Use node modules
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy({
+    usernameField: 'email'
+  },
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+
 
 // ******* MODELS ********
 
@@ -44,11 +86,6 @@ connection
     console.error('Unable to connect to the database:', err);
   });
 
-// Use node modules
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
-
 // ******* TABLE DATA *******
 
 // FIGURE OUT HOW TO REMOVE THIS SO WE DON'T CREATE A NEW USER EVERY TIME THE APP STARTS
@@ -63,6 +100,62 @@ connection.sync();
 // 		password: '123'
 // 	});
 // });
+
+
+// ******* ROUTES ********
+
+// Root goes to sign-in page with sign-up button
+app.get("/", function(req, res) {
+	res.render("login");
+});
+
+app.post('/', passport.authenticate('local', {
+	successRedirect: '/home',
+	failureRedirect: '/',
+	failureFlash: true })
+);
+
+app.get("/signup", function(req, res) {
+	res.render("signup");
+})
+
+app.get("/home", function(req, res) {
+	res.render("home", {user: user, vendors:vendors});
+});
+
+app.post("/home", function(req, res) {
+	var first = req.body.first;
+	var last = req.body.last;
+	var email = req.body.email;
+	var tapNum = req.body.tapNum;
+	var password = req.body.password;
+	var newUser = {first: first, last: last, email: email, tapNum: tapNum, pointsBalance: 0, password: password};
+
+	User.create(newUser, function(err, addedUser) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.redirect("/home");
+		}
+	});
+});
+
+app.get("/myrewards", function(req, res) {
+	res.render("myrewards", {user: user, vendors:vendors});
+})
+
+app.get("/earnpoints", function(req, res) {
+	res.render("earnpoints");
+})
+
+app.get("/*", function(req, res) {
+  res.render("404");
+});
+
+// Run app on localhost:3000
+app.listen(3000, function() {
+	console.log("App is running on Port 3000");
+});
 
 // ******* SEED DATA ********
 
@@ -103,58 +196,3 @@ var vendors = [
 		imgURL: "https://works-progress.com/wp-content/uploads/2015/12/menchies.png"
 	}
 ];
-
-
-// ******* ROUTES ********
-
-// Root goes to sign-in page with sign-up button
-app.get("/", function(req, res) {
-	res.render("landing");
-});
-
-// FIGURE OUT HOW TO DISPLAY THIS USER'S INFO
-app.get("/signup", function(req, res) {
-	res.render("signup");
-})
-
-app.get("/home", function(req, res) {
-	connection.sync().then(function () {
-		User.find({
-		});
-	});
-	res.render("home", {user: user, vendors:vendors});
-});
-
-app.post("/home", function(req, res) {
-	var first = req.body.first;
-	var last = req.body.last;
-	var email = req.body.email;
-	var tapNum = req.body.tapNum;
-	var password = req.body.password;
-	var newUser = {first: first, last: last, email: email, tapNum: tapNum, pointsBalance: 0, password: password};
-
-	User.create(newUser, function(err, addedUser) {
-		if (err) {
-			console.log(err);
-		} else {
-			res.redirect("/home");
-		}
-	});
-});
-
-app.get("/myrewards", function(req, res) {
-	res.render("myrewards", {user: user, vendors:vendors});
-})
-
-app.get("/earnpoints", function(req, res) {
-	res.render("earnpoints");
-})
-
-app.get("/*", function(req, res) {
-  res.render("404");
-});
-
-// Run app on localhost:3000
-app.listen(3000, function() {
-	console.log("App is running on Port 3000");
-});
