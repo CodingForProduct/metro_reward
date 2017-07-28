45f7c6150f77548b858d189360574681ab729d72
badickens <badckns@yahoo.com>
2017-07-28 10:28:03 -0700

    merged from remote master to add in login code

---
+++ public/js/calcs.js
@@ -0,0 +1,12 @@
+var getBalance = function() {
+    var totalPoints = document.getElementsByClassName("circle");
+    var balance = totalPoints[0].innerHTML -
+    console.log(balance);
+
+    /*
+    connection.sync().then(function( {
+        Vendor.findById()
+
+    });*/
+        return true;
+    };
