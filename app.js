eba124bb81fc8fc31f16de687823ff2fea8f264f
kammitama5 <krystal.maughan@gmail.com>
2017-07-30 01:18:55 +0000

    amend process.env.config

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var User = require("./models/user");
 // var sessionStore = new SequelizeStore();
 
 // Use node modules
-if (NPM_CONFIG_PRODUCTION != "true"){
+if (process.env.NPM_CONFIG_PRODUCTION != "true"){
   require('dotenv').config()
 }
 app.set("view engine", "ejs");
