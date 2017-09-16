2fde5bf501e6c546d2b4e77b4199ab0a0b3668b8
niamurrell <niamurrell@users.noreply.github.com>
2017-09-15 20:22:48 -0700

    Refactor user model

--- app.js
+++ app.js
@@ -9,9 +9,60 @@ var expressValidator = require('express-validator');
 var flash = require("connect-flash");
 var session = require("express-session");
 var passportLocalSequelize = require("passport-local-sequelize");
-var User = require("./models/user");
+// var User = require("./models/user");
 // var SequelizeStore = require('connect-session-sequelize')(session.Store);
 // var sessionStore = new SequelizeStore();
+
+// Connect to postgres db via sequelize
+var connection = new Sequelize(process.env.PGDATABASE, process.env.PGUSER, process.env.PGPASSWORD, {
+  host: process.env.PGHOST,
+  dialect: "postgres",
+  port: process.env.PGPORT,
+  pool: {
+   max: 5,
+   min: 0,
+   idle: 10000
+ }
+});
+
+connection
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch(err => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+  // ******* Postgres MODELS ********
+  var User = connection.define("user", {
+    first: Sequelize.STRING,
+    last: Sequelize.STRING,
+    username: Sequelize.STRING,
+    pointsBalance: Sequelize.INTEGER,
+    tapNum: Sequelize.STRING,
+    myhash: Sequelize.TEXT,
+    mysalt: Sequelize.STRING
+  });
+
+  var Vendor = connection.define("vendor", {
+    name: Sequelize.STRING,
+    reward: Sequelize.STRING,
+    pointsNeeded: Sequelize.INTEGER,
+    imgURL: Sequelize.STRING
+  });
+
+// Activate passport-local-sequelize
+passportLocalSequelize.attachToUser(User, {
+    usernameField: "username",
+    hashField: "myhash",
+    saltField: "mysalt"
+});
+
+
+// Create table in database if not exists
+connection.sync();
+
 
 // Use node modules
 if (process.env.NODE_ENV != "production"){
